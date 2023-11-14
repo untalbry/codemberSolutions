727cab85ac03fc61f3969f0cbc635f4c5908d93d
xVrzBx <bryancsinformatica@gmail.com>
2023-11-14 14:56:50 -0600

    Added challenge 02 solution

--- CHALLENGE_02/challenge02.java
+++ CHALLENGE_02/challenge02.java
@@ -1,5 +1,39 @@
 package CHALLENGE_02;
 
 public class challenge02 {
-    
+    public static void main(String[] args) {
+        challenge(
+                "&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&");
+    }
+
+    /**
+     * 
+     * @param txt
+     * @return operations result
+     *         # add 1
+     * @ subtract 1
+     * * Multiply by it self
+     * & print the current numeric value
+     */
+    public static void challenge(String txt) {
+        int number = 0;
+        for (char c : txt.toCharArray()) {
+            switch (c) {
+                case '#':
+                    number++;
+                    break;
+                case '@':
+                    number--;
+                    break;
+                case '*':
+                    number *= number;
+                    break;
+                case '&':
+                    System.out.print(number);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
